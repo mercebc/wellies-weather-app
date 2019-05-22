5e17582dbe808bcd33492c8964de4ff0a6d39d69
Merce Bauza <miss.merce@jsuchy.localdomain>
2019-05-22 11:20:39 +0100

    remove hourly tests

--- test/wellies_web/api_requests/open_weather_test.exs
+++ test/wellies_web/api_requests/open_weather_test.exs
@@ -9,12 +9,6 @@ defmodule WelliesWeb.WeatherRequestApiTest do
     assert ResponseHandler.get_status_line(response) == {'HTTP/1.1', 200, 'OK'}
   end
 
-  test "request 12h weather in london and get success response" do
-    london = "London"
-    response = hourly_in(london)
-    assert ResponseHandler.get_status_line(response) == {'HTTP/1.1', 200, 'OK'}
-  end
-
   test "request 5 days weather in london and get success response" do
     london = "London"
     response = next_five_days_in(london)
