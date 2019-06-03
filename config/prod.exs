153213a1e838e6f0446b383de1e6e40192f058c1
Merce Bauza <mbauza@8thlight.com>
2019-06-03 11:42:14 +0100

    adds a comma

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 config :wellies, WelliesWeb.Endpoint,
   url: [scheme: "https", host: "shrouded-tundra-22745.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
