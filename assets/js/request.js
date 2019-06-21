62345faf744265f9da69203f8d7d0018f9fd5999
Merce Bauza <mbauza@8thlight.com>
2019-06-21 12:47:13 +0100

    refactor request introducing httpCLientWrapper

--- assets/js/request.js
+++ assets/js/request.js
@@ -1,6 +1,16 @@
-const request = (url) => {
-  return fetch(url)
-    .then(res => res.json())
-};
+import createHTTPClient from './httpCLient';
+import { api_key } from './api_key';
+import createConnection from './httpClient';
+import httpClientWrapper from './httpClientWrapper';
 
-export default request;
+const API_KEY = api_key;
+const API_ROOT = 'https://api.openweathermap.org/data/2.5';
+
+const connection = createConnection(httpClientWrapper);
+
+export function url(city) { return `${API_ROOT}/find?q=${city}&APPID=${API_KEY}` };
+
+export default function request(city) {
+  const APIUrl = url(city);
+  return connection(APIUrl); //returns a promise
+};
